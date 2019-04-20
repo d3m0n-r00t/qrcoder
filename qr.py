f722029de9957bc473b5018bfb10e3e26eac722e
d3m0nr007 <sreekanthroot@gmail.com>
2019-04-20 12:56:06 +0530

    first commit

---
+++ qr.py
@@ -0,0 +1,32 @@
+from qrtools import QR
+import sys
+import os
+from optparse import OptionParser
+
+def qr_encode(option, opt_str, value, parser):
+	text=sys.argv[2]
+	img=QR(data=text,pixel_size=10)
+	img.encode()
+ 	os.system("mv "+img.filename+"~/Desktop")
+ 	print("File is saved in Desktop")
+
+
+def qr_decode(option, opt_str, value, parser):
+	file=sys.argv[2]
+	img=QR(filename=file)
+	img.decode()
+	print("Decode Complete")
+	print(img.data)	
+
+
+def main():
+	parser=OptionParser(usage="usage: qr.py [options] required text / filepath")
+	parser.add_option("-e", "--encode",action="callback",callback=qr_encode,help="Encode the given text into QR code.",metavar="Text")
+	parser.add_option("-d","--decode",action="callback",callback=qr_decode,help="Decode the given QR code.",metavar="FILE")
+	(options, args)=parser.parse_args()
+
+
+if __name__=="__main__":
+	main()
+
+
